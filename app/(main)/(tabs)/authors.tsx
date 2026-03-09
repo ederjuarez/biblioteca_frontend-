2f1fb86e438c4e6440ac21d7162b8517906be59e
Eder Juarez <eder.juarez@ingesoft.com.mx>
2026-03-09 16:45:37 -0600

    paginación de autores en la app

--- app/(main)/(tabs)/authors.tsx
+++ app/(main)/(tabs)/authors.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, StatusBar, TouchableOpacity } from "react-native";
-import { List, Text, MD3Colors } from "react-native-paper";
+import { StyleSheet, StatusBar, FlatList } from "react-native";
+import { List, Text } from "react-native-paper";
 import api from "@/api/axios";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -14,36 +14,40 @@ interface Author {
 
 export default function Authors() {
   const [authors, setAuthors] = useState<Author[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [hasMore, setHasMore] = useState("");
+  console.log("Current page:", currentPage);
 
   useEffect(() => {
     try {
-      api.get("/authors/").then((response) => {
-        setAuthors(response.data.results);
-      });
+      api
+        .get("/authors/", {
+          params: {
+            page: currentPage,
+          },
+        })
+        .then((response) => {
+          setAuthors((prev) => [...prev, ...response.data.results]);
+          console.log("Fetched authors =>", response.data.links.next);
+          setHasMore(response.data.links.next);
+        });
     } catch (error) {
       console.log("Error fetching authors =>", error);
     }
-  }, []);
+  }, [currentPage]);
 
   return (
     <SafeAreaView style={styles.container}>
       <Text variant="titleLarge">Lista de autores</Text>
-      {authors.map((author) => (
-        <ListItem
-          key={author.id}
-          title={author.name}
-          description={author.birth_date}
-          right={(props) => (
-            <TouchableOpacity onPress={() => console.log("Ir a libros")}>
-              <List.Icon
-                {...props}
-                color={MD3Colors.primary60}
-                icon="bookshelf"
-              />
-            </TouchableOpacity>
-          )}
-        />
-      ))}
+      <FlatList
+        data={authors}
+        renderItem={({ item }) => (
+          <ListItem title={item.name} description={item.birth_date} />
+        )}
+        keyExtractor={(item) => item.id.toString()}
+        onEndReached={() => hasMore && setCurrentPage((prev) => prev + 1)}
+        onEndReachedThreshold={0.5}
+      />
     </SafeAreaView>
   );
 }
